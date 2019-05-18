e9ced56328111cc4889ef9c8a3477a914c3de106
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2019-05-18 15:47:19 +0200

    Add Net472 example

---
+++ PetaPocoExample.Net472/Program.cs
@@ -0,0 +1,32 @@
+﻿using PetaPocoExample.Net472.Entities;
+using System.Linq;
+
+namespace PetaPocoExample.Net472
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new Northwind();
+
+            var orders = db.Query<Order>("").ToArray();
+
+            var sql = PetaPoco.Sql.Builder
+                .Append("SELECT o.*, e.*")
+                .Append("FROM Orders as o")
+                .Append("LEFT JOIN Employees as e ON e.EmployeeID = o.EmployeeID");
+
+            var ordersJoinedWithEmployees = db.Query<Order, Employee, (Order, Employee)>((order, employee) => (order, employee), sql).ToList();
+
+
+            var sql2 = PetaPoco.Sql.Builder
+                .Append("SELECT o.*, e.*")
+                .From("Orders as o")
+                .LeftJoin("Employees as e").On("e.EmployeeID = o.EmployeeID");
+
+            var ordersJoinedWithEmployees2 = db.Query<Order, Employee, (Order, Employee)>((order, employee) => (order, employee), sql2).ToList();
+
+            int x = 0;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # PetaPocoExample
 PetaPoco example project using Northwind Database
+
+## PetaPoco
+* https://github.com/CollaboratingPlatypus/PetaPoco
+
+## SQL Example Database : Northwind
+* https://github.com/microsoft/sql-server-samples/tree/master/samples/databases/northwind-pubs
